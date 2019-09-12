e0ee16a4cdb6780065c5e6d02d87f98d28180c11
thanhkieuus <voltakieu@yahoo.com>
2019-09-11 22:43:31 -0500

    First Commit

---
+++ vk-discovery-server/src/main/java/com/vk/discovery/VkDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.vk.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkDiscoveryServerApplication.class, args);
+	}
+
+}
